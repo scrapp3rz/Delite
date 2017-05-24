b2a5434979e86e6af6d6eee40eacd0e03ec71756
vincent <portabledevincent@gmail.com>
2017-05-24 10:40:37 +0200

    pageAccueil

---
+++ pageAccueil.php
@@ -0,0 +1,89 @@
+<?php include ("entete.php");?>
+
+
+   <h1><strong>DeLight</strong></h1>
+
+
+<nav class="navbar">
+
+    <ul class="navnavbar">
+      
+<div id="accueilligne1"> accueildiv1
+          
+
+
+
+      
+
+            <li class= "menu">
+                
+                <a href="pageStarter.php"  ><img src="media/entree.svg" alt="Entrées" ></a>
+                 </li>
+
+       
+            <li class= "menu">
+                
+                <a href="pageDish.php"><img src="media/plat.svg"alt="Plats" ></a>
+                 </li>
+
+                 
+            <li class= "menu">
+                
+                <a href="pageDessert.php"><img src="media/dessert.svg"alt="Desserts" ></a>
+                 </li>
+
+</div>
+
+<div id="accueilligne2"> accueildiv2
+    
+            <li class= "menu">
+                
+                <a href="pageDrink.php"><img src="media/boisson.svg"alt="Boissons" ></a>
+                 </li>
+
+
+
+            <li class= "menu">
+               
+                <a href="pageAccueil.php"><img src="media/accueil.svg"alt="accueil" ></a>
+                 </li>
+
+
+
+            <li class= "menu">
+                
+                <a href="pageSmartCook.php"><img src="media/smartCook.svg"alt="SmartCook" ></a>
+                 </li>
+
+</div>
+
+<div id="accueilligne3"> accueildiv3
+    
+            <li class= "menu">
+                
+                <a href="pageNewRecipe.php"><img src="media/newrecipe.svg"alt="Nouvelle Recette" ></a>
+                 </li>
+
+            <li class= "menu">
+                
+                <a href="pagePerfectEgg.php"><img src="media/egg.svg"alt="oeuf parfait" ></a>
+                 </li>
+
+            <li class= "menu">
+                
+                <a href="pageConversion.php"><img src="media/convert.svg"alt="conversion" ></a>
+                 </li>
+
+</div>
+
+</ul>
+</nav>
+
+
+
+
+
+
+
+
+<?php include("pieddepage.php"); ?>
