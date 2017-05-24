d876c7a1ffab590fbd823446a061dee704caca46
vincent <portabledevincent@gmail.com>
2017-05-24 11:19:09 +0200

    creation menuTop voir à le garder ou non

---
+++ menuTop.php
@@ -0,0 +1,64 @@
+
+<nav class="menutop">
+
+    <ul class="menutoptop">
+
+        
+        <li class= "menu">
+            <a href="pageAccueil.php">
+                Accueil
+            </a>
+         </li>
+
+        </li>
+      <li class= "menu">
+            <a href="pageStarter.php">
+                Entrées
+            </a>
+        </li>
+
+        <li class= "menu">
+            <a href="pageDish.php">
+                Plats
+            </a>
+        </li>
+
+        <li class= "menu">
+            <a href="pageDessert.php">
+                Desserts
+            </a>
+        </li>
+
+        <li class= "menu">
+            <a href="pageDrink.php">
+                Boissons
+            </a>
+        </li>
+
+        <li class= "menu">
+             <a href="pageSmartCook.php">
+                SmartCook
+              </a>
+        </li>
+
+        <li class= "menu">
+            <a href="pageVegetarien.php">
+                Végétariens
+            </a>
+        </li>    
+
+        <li class= "menu">
+            <a href="pagePerfectEgg.php">
+                L'oeuf parfait
+            </a>
+        </li> 
+
+        </li>   
+        <li class= "menu">
+            <a href="pageConversion.php">
+                Conversion
+            </a>
+        </li> 
+  
+</ul>
+</nav>
