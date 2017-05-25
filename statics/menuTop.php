20774d82b48dc7254b7e1e5fbaa70182cdff238b
vincent <portabledevincent@gmail.com>
2017-05-25 17:46:45 +0200

    suppr navbar top tmp

--- statics/menuTop.php
+++ statics/menuTop.php
@@ -1,65 +1,63 @@
 
-<div class="navbar navbar-inverse navbar-fixed-top">
-   <div class="navbar-header">
-       
-   </div>
-    <ul class="navbar-nav">
-
-        <li class= "active">
+<div class="navbar navbar-default">
+   
+    <ul class="menutoptop">
+         
+        <li class= "menu">
             <a href="pageAccueil.php">
                 Accueil
             </a>
          </li>
 
         </li>
-      <li class= "active">
+      <li class= "menu">
             <a href="pageStarter.php">
                 Entrées
             </a>
         </li>
 
-        <li class= "active">
+        <li class= "menu">
             <a href="pageDish.php">
                 Plats
             </a>
         </li>
 
-        <li class= "active">
+        <li class= "menu">
             <a href="pageDessert.php">
                 Desserts
             </a>
         </li>
 
-        <li class= "active">
+        <li class= "menu">
             <a href="pageDrink.php">
                 Boissons
             </a>
         </li>
 
-        <li class= "active">
+        <li class= "menu">
              <a href="pageSmartCook.php">
                 SmartCook
               </a>
         </li>
 
-        <li class= "active">
+        <li class= "menu">
             <a href="pageVegetarien.php">
                 Végétariens
             </a>
         </li>    
 
-        <li class= "active">
+        <li class= "menu">
             <a href="pagePerfectEgg.php">
                 L'oeuf parfait
             </a>
         </li> 
 
         </li>   
-        <li class= "active">
+        <li class= "menu">
             <a href="pageConversion.php">
                 Conversion
             </a>
         </li> 
   
 </ul>
-</div>
+</nav>
