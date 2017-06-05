fdbcf42472f7d1ebadd61690b6030fa1be667972
vincent <portabledevincent@gmail.com>
2017-06-05 20:41:45 +0200

    maj backup 050617 20:40

--- about.php
+++ about.php
@@ -3,9 +3,41 @@
 
 
 
-<div class="container-fluid">
-    descriptif du projet Delight
-</div>
+<div class="container">
+
+
+
+<p class ="col-md-4 descrProjet"> 
+Delight est une application web dont le but est d’aider les utilisateurs à cuisiner plus
+facilement, plus rapidement, et avec moins de gâchis. <br>
+L’application part du principe que, lorsque l’on cuisine, les recettes sont la plupart
+du temps indiquées pour un nombre de personnes fixées, et que cette recette
+donnera un bon résultat, mais sera rarement adaptée aux nombre de personnes
+qui la consommeront. Il y aura donc des restes, lesquels finiront à la poubelle, ou
+encombreront le frigo. <br>
+L’objectif est donc de proposer une liste de recettes simplifiées (30 pour le
+moment), et qui s’adaptent automatiquement à l’aide d’un curseur, de 1 à 50
+personnes. <br>
+Pour faciliter l’usage, le texte est réduit au minimum de manière à pouvoir être
+utilisé sur smartphone, tablette, aussi bien que sur ordinateur, et d’arriver à la
+recette voulue et adaptée en 3 clics <br>
+
+</p>
+
+<p class ="col-md-4 descrProjet"> 
+    Delight is a web application which objective is to cook more easily, faster, and with
+less food mess. <br>
+The application is based on the principle that when you’re cooking, the recipes are
+mostly rated for a fixed number of guests. This recipe will give you a good result,
+but will rarely be adapted to the number of people at the table. So there will be
+leftovers, which will end up in the trash or in the fridge. <br>
+The purpose is therefore to propose a list of simplified recipes (30 right now),
+which automatically adapt on a click, from 1 to 50 people. <br>
+For making it easy to use, the instruction are reduced to a minimum so that it can
+be easily read on smartphones, tablets, as well as on computers.<br> <br>
+Your adapted recipe is only three clicks away. <br>
+    
+      </p>
 
 
 
