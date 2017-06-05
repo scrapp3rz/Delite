676bbcae3b406d956186837a035c1f103d9e520f
vincent <portabledevincent@gmail.com>
2017-06-05 18:57:33 +0200

    ajout des script JS et  lien Jquery

--- entete.php
+++ entete.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"> 
         <link href="assets/css/style.css" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="assets/js/script.js"></script>
         
     </head>
     <div class="header" >
