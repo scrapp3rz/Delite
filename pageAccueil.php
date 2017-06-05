ea945fd62463ccb44eb2469cf76b0c14554d16fb
vincent <portabledevincent@gmail.com>
2017-06-05 21:03:21 +0200

    nettoyage commentaires Accueil

--- pageAccueil.php
+++ pageAccueil.php
@@ -5,8 +5,7 @@
     <div class="col-md-4">
         <div class="cat-recipe entree">
             <a href="pageStarter.php">
-                <!--<img src="assets/images/entree.png" alt="Entrées" >-->
-                <h2>Entrées</h2>
+         <h2>Entrées</h2>
             </a>
         </div>
     </div>
@@ -14,8 +13,7 @@
     <div class="col-md-4">
         <div class="cat-recipe plat">
             <a href="pageDish.php">
-                <!--<img src="assets/images/plat.png" alt="Plats">-->
-                <h2>Plats</h2>
+         <h2>Plats</h2>
             </a>
         </div>
     </div>
@@ -23,8 +21,7 @@
     <div class="col-md-4">
         <div class="cat-recipe dessert">
             <a href="pageDessert.php">
-                <!--<img src="assets/images/dessert.png" alt="Desserts">-->
-                <h2>Desserts</h2>
+        <h2>Desserts</h2>
             </a>
         </div>
     </div>
@@ -33,8 +30,7 @@
     <div class="col-md-4">
         <div class="cat-recipe vegetarien">
             <a href="pageVegetarien.php">
-                <!--<img src="assets/images/vegetarien.png" alt="Végétarien">-->
-                <h2>Végétarien</h2>
+        <h2>Végétarien</h2>
             </a>
         </div>
     </div>
@@ -42,8 +38,7 @@
     <div class="col-md-4">
         <div class="cat-recipe chrono">
             <a href="page15min.php">
-                <!--<img src="assets/images/15min.png" alt="15 minutes chrono">-->
-                <h2>15 min'</h2>
+        <h2>15 min'</h2>
             </a>
         </div>
     </div>
@@ -52,8 +47,7 @@
     <div class="col-md-4">
         <div class="cat-recipe smartcook">
             <a href="pagesmartCook.php">
-                <!--<img src="assets/images/smartCook.png" alt="xsartCook">-->
-                <h2>SmartCook</h2>
+        <h2>SmartCook</h2>
             </a>
         </div>
     </div>
@@ -63,16 +57,14 @@
     <div class="col-md-4">
         <div class="cat-recipe timer">
             <a href="pageTimer.php">
-                <!--<img src="assets/images/minuteur.png" alt="oeuf parfait">-->
-                <h2>Minuteur</h2>
+        <h2>Minuteur</h2>
             </a>
         </div>
     </div>
     <div class="col-md-4">
         <div class="cat-recipe drink">
             <a href="pageDrink.php">
-                <!--<img src="assets/images/boisson.png" alt="Boissons">-->
-                <h2>Boissons</h2>
+        <h2>Boissons</h2>
             </a>
         </div>
     </div>
@@ -81,8 +73,7 @@
     <div class="col-md-4">
         <div class="cat-recipe conversion">
             <a href="pageConversion.php">
-                <!--<img src="assets/images/conversion.png" alt="conversion">-->
-                <h2>Conversion</h2>
+        <h2>Conversion</h2>
             </a>
         </div>
     </div>
