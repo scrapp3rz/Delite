b34b4f628d7eeb1081edf8213de9e367f0bc953d
vincent <portabledevincent@gmail.com>
2017-06-06 22:25:48 +0200

    suppression footer dans page recette pour uniformité de page

--- pageRecette.php
+++ pageRecette.php
@@ -26,26 +26,26 @@ $data = recipeFor2b($id_recette,$numberToSubmit);
 ?>
 
 
- <div class="div-couleur0 col-md-4">
+ <div class="div-couleur0 container-fluid">
      <div class="col-xs-12 enCuisine">
             
   <!--  affichage de la recette, titre, quantités initialisées à 1 -->
 
-<form method="GET">Recette pour   
-<input type="number" name="numberToSubmit" class="numberToCookFor" value="<?php echo $numberToSubmit; ?>" min="1" max="50">
-<button type="submit" class="btn btn-outline-success numberToCookFor2">GO</button>
+<form method="GET">   
+<input type="number" name="numberToSubmit" class="input-num numberToCookFor" value="<?php echo $numberToSubmit; ?>" min="1" max="50"> personnes
+<button type="submit" class="input-btn numberToCookFor2">GO</button>
 <input type="hidden" name="id_recette" value="<?php echo $id_recette; ?>">
 
         </form>
    </div>
    <p class="titrerecette">
     
-  <h1> <strong class="uppercase"> <?php echo $data[0]['nomrecette'];    ?> </strong></h1>
-</p> <br>
+  <h2> <strong class="uppercase"> <?php echo $data[0]['nomrecette'];    ?> </strong></h2>
+</p>
 
 <div class="recettePrepareTime howTo">
  <strong class="uppercase">   Préparation : </strong>
-    <?php  echo $data[0]['preparation'];  ?> minutes  
+    <?php  echo $data[0]['preparation'];  ?> minutes <br> 
  
 
  <strong class="uppercase">   Cuisson :  </strong>
@@ -55,8 +55,8 @@ $data = recipeFor2b($id_recette,$numberToSubmit);
     <?php echo   $data[0]['temperature'];  ?>
 </div> <br>
 
-
-<div class="recetteIngredient class="ingredientConsigne">
+<h4 class>
+<div class="recetteIngredient ingredientConsigne">
  <strong class="uppercase">   Ingrédients : </strong> <br> <strong>
 <?php // boucle pour afficher les ingrédients et quantité
 foreach ($data as $key => $values) {
@@ -68,7 +68,13 @@ foreach ($data as $key => $values) {
   <strong class="uppercase">  Description : </strong><br>
     <?php  echo  $data[0]['description'];  ?>
     </div> <br>
+</h4>
 
 
-<?php include("pieddepage.php"); ?>
+
+
+</body>
+</html>
+
+
 
--- piedDePage.php
+++ piedDePage.php
@@ -2,25 +2,18 @@
  <!-- ================================================
  ==========FOOTER=================================== -->
 
- 
 
 
-    <footer>
+    <footer> <h6>
         <div class="footer" id="pdp">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h1 class="navbar-brand">
-                        <a href="./"></a>
-                    </h1>  
-                    <p class="copy"><a href="about.php"><b>  <strong>A propos -</a>
+                <div class="col-md-12 col-sm-12 col-xs-12">        
+                    <p class="copy"><a href="about.php"><b>  <strong>A propos </strong>-</a>
                    &#169; <span id="copyright-year"> 2017</span>
                         <a> <b><strong>Delight</strong></b></a> - 
                          <a href="contact.php"> <b><strong>Contact</strong></b></a>
-                        <a rel="nofollow" href="   " target="le site à créer"> </a></strong></b>
                     </p>
                 </div>
-            </div>
-        </div>
+        </div></h6>
 </footer>
 
 </body>
