497e6eebf9d55611ceeff34b596d4792de851eec
Madmarsu <madmarsu@gmail.com>
2017-06-26 11:05:03 +0200

    simplification/normalisation des balises +corr. accent

--- piedDePage.php
+++ piedDePage.php
@@ -4,17 +4,17 @@
 
 
 
-    <footer> <h6>
+    <footer><h6>
         <div class="footer" id="pdp">
-                <div class="col-md-12 col-sm-12 col-xs-12">        
-                    <p class="copy"><a href="about.php"><b>  <strong>A propos </strong>-</a>
-                   &#169; <span id="copyright-year"> 2017</span>
-                        <a> <b><strong>Delight</strong></b></a> - 
-                         <a href="contact.php"> <b><strong>Contact</strong></b></a>
-                    </p>
-                </div>
+            <div class="col-md-12 col-sm-12 col-xs-12">        
+                <p class="copy"><strong><a href="about.php">&Agrave; propos</a></strong>
+                   &copy; <span id="copyright-year"> 2017</span>
+									          <strong>Delight</strong> - 
+                   <strong><a href="contact.php">Contact</a></strong>
+                </p>
+            </div>
         </div></h6>
-</footer>
+    </footer>
 
 </body>
 </html>
